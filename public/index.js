4c9fca5d63450817889b3391d258f2dc18134afb
handleryouth <rafaeltonydavid@yahoo.com>
2021-05-12 10:03:37 +0700

    server remastered

--- public/index.js
+++ public/index.js
@@ -4,14 +4,22 @@ const hamburgerIcon = document.querySelector('.hamburger-icon');
 const crossIcon = document.querySelector('.cross-icon');
 const additionalMenu = document.querySelector('.additional-menu');
 
-hamburgerIcon.addEventListener('click', function(){
+hamburgerIcon.addEventListener('click', function() {
   additionalMenu.classList.remove("notshowed");
   hamburgerIcon.classList.add("notshowed");
   crossIcon.classList.remove("notshowed");
 });
 
-crossIcon.addEventListener('click', function(){
+crossIcon.addEventListener('click', function() {
   additionalMenu.classList.add("notshowed");
   hamburgerIcon.classList.remove("notshowed");
   crossIcon.classList.add("notshowed");
+});
+
+const copyButton = document.querySelector(".copy-button");
+
+copyButton.addEventListener('click', function() {
+  var copiedText = document.querySelector(".shortened-link-properties");
+  copiedText.select();
+  document.execCommand('Copy');
 });
