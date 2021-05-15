92d05571bdd7a1b9e8b950c8d0a0c18948075bc0
handleryouth <rafaeltonydavid@yahoo.com>
2021-05-16 00:28:53 +0700

    fix bug

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let alert = require('alert');
 
 var listShortenLinks = [];
 var listOriginalLinks = [];
+var listTargetLinks = [];
 errorMessage = "opps, blacklist domain! Try other link";
 
 app.use(bodyParser.urlencoded({
@@ -19,58 +20,66 @@ app.set('view engine', 'ejs');
 app.engine('html', require("ejs").renderFile);
 
 app.get("/", function(req, res) {
-  listShortenLinks.splice(0, listShortenLinks.length);
-  listOriginalLinks.splice(0, listOriginalLinks.length);
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post('/', function(req, res) {
   var targetLink = req.body.linkObject;
-  const url = "https://api.shrtco.de/v2/shorten?url=" + targetLink;
-  https.get(url, function(response) {
-    response.on("data", function(data, error) {
-      try {
-        const linkData = JSON.parse(data);
-        linkData.toString().replace('/', '');
+  if(targetLink === listTargetLinks[listTargetLinks.length - 1]){
+    res.render("index1", {
+      shortlink: listShortenLinks,
+      originallink: listOriginalLinks
+    });
+  }
+  else{
+    listTargetLinks.push(targetLink);
+    const url = "https://api.shrtco.de/v2/shorten?url=" + targetLink;
+    https.get(url, function(response) {
+      response.on("data", function(data, error) {
         try {
-          var completeTargetLink = targetLink;
-          if (completeTargetLink !== listOriginalLinks[(listOriginalLinks.length - 1)]) {
-            if (completeTargetLink.length > 25) {
-              completeTargetLink = completeTargetLink.substring(0, 50) + "...";
-            }
-            listOriginalLinks.push(completeTargetLink);
-            const shortenLink = linkData.result.short_link;
-            var completeShortenLink = shortenLink;
+          const linkData = JSON.parse(data);
+          linkData.toString().replace('/', '');
+          try {
+            var completeTargetLink = targetLink;
+            if (completeTargetLink !== listOriginalLinks[(listOriginalLinks.length - 1)]) {
+              if (completeTargetLink.length > 25) {
+                completeTargetLink = completeTargetLink.substring(0, 50) + "...";
+              }
+              listOriginalLinks.push(completeTargetLink);
+              const shortenLink = linkData.result.short_link;
+              var completeShortenLink = shortenLink;
 
-            listShortenLinks.push(completeShortenLink);
-            res.render("index1", {
-              shortlink: listShortenLinks,
-              originallink: listOriginalLinks
-            });
-          } else {
-            res.render("index1", {
-              shortlink: listShortenLinks,
-              originallink: listOriginalLinks
-            });
+              listShortenLinks.push(completeShortenLink);
+              res.render("index1", {
+                shortlink: listShortenLinks,
+                originallink: listOriginalLinks
+              });
+            } else {
+              res.render("index1", {
+                shortlink: listShortenLinks,
+                originallink: listOriginalLinks
+              });
+            }
+          } catch (e) {
+            listShortenLinks.push(errorMessage);
+
           }
         } catch (e) {
-          listShortenLinks.push(errorMessage);
+          if (targetLink.length > 25) {
+            targetLink = targetLink.substring(0, 50) + "...";
+          }
+          listOriginalLinks.push(targetLink);
+          listShortenLinks.push("the link cannot be shorten");
+          res.render("index1", {
+            shortlink: listShortenLinks,
+            originallink: listOriginalLinks
+          });
+        }
+      });
 
-        }
-      } catch (e) {
-        if (targetLink.length > 25) {
-          targetLink = targetLink.substring(0, 50) + "...";
-        }
-        listOriginalLinks.push(targetLink);
-        listShortenLinks.push("the link cannot be shorten");
-        res.render("index1", {
-          shortlink: listShortenLinks,
-          originallink: listOriginalLinks
-        });
-      }
     });
+  }
 
-  });
 });
 
 app.listen(process.env.PORT || 3000, function() {
